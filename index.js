3593dca6b9657bb9e88304e8ac932ac59f99cf74
Jaskiran Singh <jsingh8986@yahoo.com>
2022-12-13 19:56:24 -0800

    commit tree 4895f56de5c0b04e170d01c6f52ee93173cad25e

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {return 'This one is on me!'} 
+  else if (distance > 400 && distance < 2000) {return 'That will be twenty bucks.' } 
+  else if (2000 < distance && 2500 > distance) {return 'I will gladly take your thirty bucks.'} 
+  else if (distance >= 2500) {return 'No can do.'}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return 'NYC' == city ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(amountOfTip){
+  switch (amountOfTip) {
+    case 'generous': 
+      return('Thank you so much.');
+      break;
+    case 'not as generous':
+     return ('Thank you.');
+      break;
+    case 'thanks for everything':
+     return ('Bye.')
+    break;
+  }
 }
